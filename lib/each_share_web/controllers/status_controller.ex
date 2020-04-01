60c0efb79c81655c3ca13dd6b6544f2a93a3a8c3
Vinicius Roland <vinicius.roland@btgpactual.com>
2020-04-01 15:39:26 -0300

    update message

--- lib/each_share_web/controllers/status_controller.ex
+++ lib/each_share_web/controllers/status_controller.ex
@@ -1,6 +1,6 @@
 defmodule EachShareWeb.StatusController do
   use EachShareWeb, :controller
   def check_status(conn, _params) do
-    json(conn, %{"status" => "server up and running!"})
+    json(conn, %{"status" => "server up@"})
   end
 end
